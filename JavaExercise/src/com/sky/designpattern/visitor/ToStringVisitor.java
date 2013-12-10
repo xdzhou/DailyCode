9c9fa4f19264af6a5cb989070467fc3d99fc15c7
xdzhou <xdzhou.loic@gmail.com>
2013-12-10 16:41:30 +0100

    visitor design pattern added

---
+++ JavaExercise/src/com/sky/designpattern/visitor/ToStringVisitor.java
@@ -0,0 +1,18 @@
+package com.sky.designpattern.visitor;
+
+//this visitor used to print a string
+public class ToStringVisitor implements Visitor {
+
+	@Override
+	public void visit(ElementA ea) {
+		System.out.println("Element A");
+
+	}
+
+	@Override
+	public void visit(ElementB eb) {
+		System.out.println("Element B");
+
+	}
+
+}
---
+++ JavaExercise/src/com/sky/designpattern/visitor/Visitor.java
@@ -0,0 +1,9 @@
+package com.sky.designpattern.visitor;
+
+//every visitor has a function special
+public interface Visitor {
+	
+	public void visit(ElementA ea);
+	public void visit(ElementB eb);
+
+}
