03064fef8d84f31a88149a074731b401ea0cf4f5
xdzhou <xdzhou.loic@gmail.com>
2013-12-22 00:06:42 +0100

    union-find algo added

--- AlgoLibrary/src/main/java/com/loic/mapreduce/WordsCount.java
+++ AlgoLibrary/src/main/java/com/loic/mapreduce/WordsCount.java
@@ -2,7 +2,7 @@ package com.loic.mapreduce;
 
 import java.io.IOException;
 import java.util.*;
-     
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
---
+++ AlgoLibrary/src/test/java/com/loic/algo/UnionFindTest.java
@@ -0,0 +1,20 @@
+package com.loic.algo;
+
+import java.util.Scanner;
+
+public class UnionFindTest {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		UnionFind uf = new UnionFind(N);
+		while(true){
+			int p = in.nextInt();
+			if(p  == -1) break;
+			int q = in.nextInt();
+			uf.union(p, q);
+		}
+		System.out.println(uf.getCount()+" components");
+	}
+
+}
