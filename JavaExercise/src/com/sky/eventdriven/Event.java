6d5e7351a5194d143cb634e99d967dc4e889978d
xiandong zhou <xdzhou.loic@gmail.com>
2013-12-22 20:50:40 +0100

    event driven added

---
+++ JavaExercise/src/com/sky/eventdriven/Event.java
@@ -0,0 +1,43 @@
+package com.sky.eventdriven;
+
+public class Event implements Comparable<Event>{
+	private int time;
+	private int type; //0 arrive 1 leave,
+	private Client client;
+	
+	public Event(int type, Client client) {		
+		this.type = type;
+		if(type == 0) this.time = client.getArriveTime();
+		else this.time = client.getServiceDuring()+client.getStartServiceTime();
+		this.client = client;
+	}
+	public Event(int type, Client client, int time) {		
+		this.type = type;
+		this.time = time;
+		this.client = client;
+	}
+
+	@Override
+	public int compareTo(Event e) {
+		return this.time - e.time;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [time=" + time + ", type=" + type + ", client=" + client.getId()
+				+ "]";
+	}
+	
+}
