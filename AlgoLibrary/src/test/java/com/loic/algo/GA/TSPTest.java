9b1d9b88a3f2ed0d7ffdec7e17f3775996c325f2
xiandong zhou <xdzhou.loic@gmail.com>
2014-02-21 00:34:57 +0100

    Genetic Algorithm - TSP problem added

---
+++ AlgoLibrary/src/test/java/com/loic/algo/GA/TSPTest.java
@@ -0,0 +1,11 @@
+package com.loic.algo.GA;
+
+public class TSPTest {
+	public static void main(String[] args) {
+		int[] x = {0,1,2,2,2,1,0,0};
+		int[] y = {0,0,0,1,2,2,2,1};
+		TSP tsp = new TSP();
+		tsp.start(x, y);
+	}
+
+}
