18e9b1ebce7a7b4aa98695f37b142a125d6bb53e
Xiandong ZHOU <xdzhou.loic@gmail.com>
2014-04-29 15:32:54 +0200

    Update ChargingChaos.java

--- JavaExercise/src/com/sky/codejam/training/ChargingChaos.java
+++ JavaExercise/src/com/sky/codejam/training/ChargingChaos.java
@@ -1,6 +1,7 @@
 package com.sky.codejam.training;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -9,12 +10,14 @@ public class ChargingChaos {
 
 	public static void main(String[] args) {
 		new ChargingChaos().start();
+
 	}
 	
 	public void start(){
 		Scanner in = new Scanner(System.in);
 		int nbCase = in.nextInt();
-		BREAKFLAG: for(int i=0; i<nbCase; i++){
+		BREAKFLAG: 
+		for(int i=0; i<nbCase; i++){
 			int nbSwitch = 0;
 			int N = in.nextInt();
 			int L = in.nextInt();
@@ -27,6 +30,7 @@ public class ChargingChaos {
 			for(int j=0; j<N; j++) device.add(Integer.valueOf(devStr[j], 2));
 			int[] elcNBone = getNbOne(electric, L);
 			int[] devNBone = getNbOne(device, L);
+
 			List<Integer> specialIndex = new ArrayList<Integer>();
 			for(int j=0; j<L; j++){
 				if(elcNBone[j] == devNBone[j]){
@@ -35,14 +39,31 @@ public class ChargingChaos {
 					bitSwitch(electric, L-1-j);
 					nbSwitch ++;
 				}else {
-					System.out.println("Case #"+(i+1)+": NOT POSSIBLE");
-					break BREAKFLAG;
+					System.out.println("Case #"+(i+1)+": NOT POSSIBLE 1");
+					continue BREAKFLAG;
 				}
 			}
+			showList(specialIndex);
 			int min = getMinSwitch(device, electric, specialIndex, L);
-			if(min==-1) System.out.println("Case #"+(i+1)+": NOT POSSIBLE");
+			if(min==-1) System.out.println("Case #"+(i+1)+": NOT POSSIBLE 2");
 			else System.out.println("Case #"+(i+1)+": "+(nbSwitch+min));
 		}
+	}
+	
+	private void showList(int[] list, int size){
+		System.out.print("list[]: ");
+		for(int k=0; k<size; k++){
+			System.out.print(list[k]+" ");
+		}
+		System.out.println();
+	}
+	
+	private void showList(List<Integer> list){
+		System.out.print("Arraylist: ");
+		for(int k=0; k<list.size(); k++){
+			System.out.print(list.get(k)+" ");
+		}
+		System.out.println();
 	}
 	
 	private int getMinSwitch(List<Integer> device, List<Integer> electric, List<Integer> specialIndex, int L){
@@ -50,8 +71,8 @@ public class ChargingChaos {
 		if(!isBitEqual(device, electric, specialIndex, L)) return -1;
 		
 		List<Integer> copyelectric = new ArrayList<Integer>(electric.size());
-		Collections.copy(copyelectric, electric);
-
+		copyelectric.addAll(electric);
+		
 		int index = specialIndex.remove(0);
 		int min1 = getMinSwitch(device, electric, specialIndex, L);
 		bitSwitch(copyelectric, L-1-index);
@@ -71,7 +92,7 @@ public class ChargingChaos {
 		int[] nbOne = new int[L];
 		for(int i=0; i<list.size(); i++)
 			for(int j=0; j<L; j++){
-				nbOne[j] += list.get(i) &(1<<(L-1-j));
+				nbOne[j] += (list.get(i) &(1<<(L-1-j))) >>> (L-1-j);
 			}
 		return nbOne;
 	}
@@ -87,23 +108,28 @@ public class ChargingChaos {
 		Collections.sort(l1);
 		Collections.sort(l2);
 		for(int i=0; i<l1.size(); i++){
-			if(l1.get(i) != l2.get(i)) return false;
+			if(!l1.get(i).equals(l2.get(i))){
+				//System.out.println(l1.get(i)+" != "+l2.get(i));
+				return false;
+			}
 		}
 		return true;
 	}
 	
 	private boolean isBitEqual(List<Integer> l1, List<Integer> l2, List<Integer> specialIndex, int L){
+		if(specialIndex.size() == l1.size()) return true;
 		List<Integer> copyL1 = new ArrayList<Integer>(l1.size());
-		Collections.copy(copyL1, l1);
+		copyL1.addAll(l1);
 		List<Integer> copyL2 = new ArrayList<Integer>(l2.size());
-		Collections.copy(copyL2, l2);
+		copyL2.addAll(l2);
 		for(int index: specialIndex){
 			for(int i=0; i<copyL1.size(); i++){
 				copyL1.set(i, copyL1.get(i)&(0xFFFFFFFF ^(1<<(L-1-index))));
 				copyL2.set(i, copyL2.get(i)&(0xFFFFFFFF ^(1<<(L-1-index))));
 			}
 		}
-		return isListEqual(copyL1, copyL2);
+		boolean b = isListEqual(copyL1, copyL2);
+		return b;
 	}
 
 }
