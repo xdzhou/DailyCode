a519fcaf74120300150f55e80656b560a1cd5e64
xiandong ZHOU <xdzhou.loic@gmail.com>
2015-12-01 21:51:08 +0100

    [DEV] add EightQueens algo

---
+++ JavaExercise/src/main/com/sky/recursion/EightQueens.java
@@ -0,0 +1,46 @@
+package com.sky.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sky.problem.Problem;
+
+public class EightQueens implements Problem<Void, Integer>
+{
+
+	@Override
+	public Integer resolve(Void param)
+	{
+		List<Integer> positions = new ArrayList<Integer>(8);
+        return search(0, positions);
+	}
+	
+	private int search(int curLine, List<Integer> positions)
+    {
+        if(curLine == 8)
+        {
+            System.out.println("solution : "+positions);
+            return 1;
+        }
+        int result = 0;
+        for(int i = 0; i < 8; i++)
+        {
+            boolean available = true;
+            for(int j = 0; j < positions.size() && available; j++)
+            {
+                int p = positions.get(j);
+                if(p % 8 == i || Math.abs(curLine - (p / 8)) == Math.abs(i - (p % 8)))
+                {
+                    available = false;
+                }
+            }
+            if(available)
+            {
+                positions.add(curLine * 8 + i);
+                result += search(curLine + 1, positions);
+                positions.remove(positions.size() - 1);
+            }
+        }
+        return result;
+    }
+}
