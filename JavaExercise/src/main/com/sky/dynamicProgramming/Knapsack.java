d706853e6555855f4f8fcdbbef7d9247c41ed6c6
xiandong ZHOU <xdzhou.loic@gmail.com>
2015-12-06 20:38:43 +0100

    [DEBUG] move Knapsack algo

---
+++ JavaExercise/src/main/com/sky/dynamicProgramming/Knapsack.java
@@ -0,0 +1,47 @@
+package com.sky.dynamicProgramming;
+
+import java.util.Objects;
+
+import com.google.common.base.Preconditions;
+import com.loic.algo.common.Triple;
+import com.sky.problem.Problem;
+
+/*
+ * 01背包问题:
+ * 有N件物品和一个容量为V的背包。第i件物品的费用是c[i]，价值是w[i]。
+ * 求解将哪些物品装入背包可使价值总和最大。
+ */
+public class Knapsack implements Problem<Triple<Integer, Integer[], Integer[]>, Integer>
+{
+	@Override
+	public Integer resolve(Triple<Integer, Integer[], Integer[]> param)
+	{
+		Objects.requireNonNull(param);
+		Preconditions.checkArgument(param.getSecond().length == param.getThird().length, "weight.length != value.length");
+		return getMaxValue(param.getFirst(), param.getSecond(), param.getThird());
+	}
+
+	private int getMaxValue(int capacity, Integer[] weight, Integer[] value)
+	{
+		int nbObjet = weight.length;
+		int[][] dp = new int[nbObjet + 1][capacity + 1];
+
+		for (int i = 1; i <= nbObjet; i++)
+		{
+			for (int j = 1; j <= capacity; j++)
+			{
+				if (weight[i - 1] <= j)
+				{
+					dp[i][j] = Math.max(value[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+				} 
+				else
+				{
+					dp[i][j] = dp[i - 1][j];
+				}
+			}
+		}
+		return dp[nbObjet][capacity];
+	}
+
+	
+}
