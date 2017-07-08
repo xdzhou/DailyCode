47b6371a133f3e90ec283348c2d4ea926a868882
xiandong <xdzhou.loic@gmail.com>
2017-07-08 23:08:03 +0200

    FIX pmd issue

--- AlgoLibrary/src/main/java/com/loic/algo/search/core/HeuristicStrategy.java
+++ AlgoLibrary/src/main/java/com/loic/algo/search/core/HeuristicStrategy.java
@@ -1,7 +1,7 @@
 package com.loic.algo.search.core;
 
 public interface HeuristicStrategy<State> {
-    public static final double ZERO = 0d;
+    double ZERO = 0d;
 
     double heuristic(State state, int depth);
 }
