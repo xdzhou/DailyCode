03b841a7451e263d70ab1dc3e3010c8b472d4601
xiandong <xdzhou.loic@gmail.com>
2018-05-10 22:10:13 +0200

    remove useless codes

--- Exercise/Generic/src/test/java/com/loic/PipelineItemResolver.java
+++
@@ -1,9 +0,0 @@
-package com.loic;
-
-import com.loic.algo.formula.Literal;
-
-public interface PipelineItemResolver<Item> {
-  <K> K nextLevelKey(Item item, K curKey);
-
-  Literal value(Item item);
-}
