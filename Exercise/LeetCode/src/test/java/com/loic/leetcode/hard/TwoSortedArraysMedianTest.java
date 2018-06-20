f405ced6ca52f2f9c1c785d8d00c52bb893cb67e
xiandong <xdzhou.loic@gmail.com>
2018-06-20 22:31:33 +0200

    add median @ two sorted array

---
+++ Exercise/LeetCode/src/test/java/com/loic/leetcode/hard/TwoSortedArraysMedianTest.java
@@ -0,0 +1,16 @@
+package com.loic.leetcode.hard;
+
+import com.loic.solution.BiSolutionChecker;
+import com.loic.solution.TestHelper;
+import org.junit.Test;
+
+public class TwoSortedArraysMedianTest {
+  @Test
+  public void test() {
+    BiSolutionChecker.create(new TwoSortedArraysMedian())
+      .check(TestHelper.toIntArray(1), TestHelper.toIntArray(2), 1.5d)
+      .check(TestHelper.toIntArray(1,2,3), TestHelper.toIntArray(2,3), 2d)
+      .check(TestHelper.toIntArray(1,3), TestHelper.toIntArray(2), 2d)
+      .check(TestHelper.toIntArray(1,2), TestHelper.toIntArray(3,4), 2.5d);
+  }
+}
