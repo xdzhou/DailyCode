acdc234afb87c5ab8fd08d07cbf6ec0a5a5ecc3d
xiandong <xdzhou.loic@gmail.com>
2018-12-23 17:39:57 +0100

    add Maximum tips calculator

---
+++ geeksforgeeks/src/main/java/com/loic/geeksforgeeks/MaximumTipCalculator.java
@@ -0,0 +1,58 @@
+package com.loic.geeksforgeeks;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/maximum-tip-calculator/0
+ * <p>
+ * Rahul and Ankit are the only two waiters in Royal Restaurant.
+ * Today, the restaurant received N orders. T
+ * he amount of tips may differ when handled by different waiters,
+ * if Rahul takes the ith order, he would be tipped Ai rupees and if Ankit takes this order,
+ * the tip would be Bi rupees.
+ * In order to maximize the total tip value they decided to distribute the order among themselves.
+ * One order will be handled by one person only.
+ * Also, due to time constraints Rahul cannot take more than X orders and Ankit cannot take more than Y orders.
+ * It is guaranteed that X + Y is greater than or equal to N, which means that all the orders can be handled by either Rahul or Ankit.
+ * Find out the maximum possible amount of total tip money after processing all the orders.
+ */
+public class MaximumTipCalculator {
+
+  public static int resolve(int N, int X, int Y, int[] Ai, int[] Bi) {
+    if (X > Y) {
+      return resolve(N, Y, X, Bi, Ai);
+    }
+    //Suppose I'm X
+
+    class Contribution {
+      final int myTips;
+      final int otherTips;
+
+      private Contribution(int myTips, int otherTips) {
+        this.myTips = myTips;
+        this.otherTips = otherTips;
+      }
+
+      //If I handle this order, I earn more tips than other guy
+      private boolean positiveContribution() {
+        return myTips > otherTips;
+      }
+    }
+    //sort all the contribution according the diff tips between me and Y
+    PriorityQueue<Contribution> pq = new PriorityQueue<>(N, Comparator.comparingInt(con -> con.otherTips - con.myTips));
+    IntStream.range(0, N).forEach(index -> pq.add(new Contribution(Ai[index], Bi[index])));
+    int ordreCount = 0;
+    int maxTips = 0;
+    while (!pq.isEmpty()) {
+      ordreCount++;
+      //poll will ge us the current smallest item
+      Contribution contribution = pq.poll();
+      //my order count is between N-Y and X, so I at least handle N-Y orders
+      boolean myOrdres = (ordreCount <= N - Y) || (ordreCount <= X && contribution.positiveContribution());
+      maxTips += (myOrdres ? contribution.myTips : contribution.otherTips);
+    }
+    return maxTips;
+  }
+}
---
+++ geeksforgeeks/src/test/java/com/loic/geeksforgeeks/MaximumTipCalculatorTest.java
@@ -0,0 +1,22 @@
+package com.loic.geeksforgeeks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MaximumTipCalculatorTest {
+
+  @Test
+  void simpleTest() {
+    Assertions.assertEquals(21, MaximumTipCalculator.resolve(5, 3, 3, new int[]{1, 2, 3, 4, 5}, new int[]{5, 4, 3, 2, 1}));
+  }
+
+  @Test
+  void mediumTest() {
+    Assertions.assertEquals(43, MaximumTipCalculator.resolve(8, 4, 4, new int[]{1, 4, 3, 2, 7, 5, 9, 6}, new int[]{1, 2, 3, 6, 5, 4, 9, 8}));
+    Assertions.assertEquals(160, MaximumTipCalculator.resolve(4, 4, 1, new int[]{10, 20, 30, 40}, new int[]{1, 25, 3, 100}));
+
+  }
+}
