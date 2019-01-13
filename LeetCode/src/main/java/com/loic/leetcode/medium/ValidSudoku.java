3afb0155c33433162b3d8cf94ad00a15be98e8ac
xiandong <xdzhou.loic@gmail.com>
2019-01-13 21:26:28 +0100

    review valid sudoku

--- LeetCode/src/main/java/com/loic/leetcode/medium/ValidSudoku.java
+++ LeetCode/src/main/java/com/loic/leetcode/medium/ValidSudoku.java
@@ -1,10 +1,6 @@
 package com.loic.leetcode.medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.IntStream;
+import com.loic.leetcode.helper.BooleanArray;
 
 /*
  * 36. Valid Sudoku
@@ -14,35 +10,36 @@ import java.util.stream.IntStream;
  */
 public class ValidSudoku {
 
-
   public static boolean resolve(char[][] board) {
-    Map<Character, List<Integer>> positionsMap = new HashMap<>();
-    return IntStream.range(0, 81)
-      .allMatch(pos -> {
-        int line = pos / 9;
-        int row = pos % 9;
-        return check(board[line][row], pos, positionsMap);
-      });
+    // first 81 items for row
+    // second 81 items for column
+    // last 81 items for cell
+    BooleanArray array = new BooleanArray(9 * 9 * 3);
+    // array[0]: row 0 already has '1', array[9]: row 1 already has '1'
+    // array[81]: column 0 already has '1' ...
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        char c = board[i][j];
+        if (c != '.') {
+          int num = c - '1';
+          int cell = i / 3 * 3 + j / 3;
+          if (!check(array, i * 9 + num, 81 + j * 9 + num, 162 + cell * 9 + num)) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
-  private static boolean check(Character key, int pos, Map<Character, List<Integer>> positionsMap) {
-    List<Integer> list = positionsMap.get(key);
-    boolean valid = true;
-    if (list != null) {
-      valid = list.stream().allMatch(p -> p / 9 != pos / 9 && p % 9 != pos % 9 && blockPosition(p) != blockPosition(pos));
-    } else if (key != '.') {
-      list = new ArrayList<>();
-      positionsMap.put(key, list);
+  // try set true for index, if anyone is already set, return false
+  private static boolean check(BooleanArray array, int... index) {
+    for (int i : index) {
+      if (array.get(i)) {
+        return false;
+      }
+      array.set(i);
     }
-    if (list != null) {
-      list.add(pos);
-    }
-    return valid;
-  }
-
-  private static int blockPosition(int index) {
-    int line = index / 9 / 3;
-    int row = index % 9 / 3;
-    return line * 3 + row;
+    return true;
   }
 }
