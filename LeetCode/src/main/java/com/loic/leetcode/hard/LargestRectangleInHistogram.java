4b7d3f353016bca8d05cb5e4b67873ade3905a13
xiandong <xdzhou.loic@gmail.com>
2019-03-02 23:50:19 +0100

    add LargestRectangleInHistogram other solution

--- LeetCode/src/main/java/com/loic/leetcode/hard/LargestRectangleInHistogram.java
+++ LeetCode/src/main/java/com/loic/leetcode/hard/LargestRectangleInHistogram.java
@@ -20,6 +20,68 @@ import java.util.PriorityQueue;
  */
 public final class LargestRectangleInHistogram {
 
+  /**
+   * get left and right boundary of every column
+   */
+  public static int boundaryArea(int... heights) {
+    if (heights.length == 0) {
+      return 0;
+    } else if (heights.length == 1) {
+      return heights[0];
+    }
+    int[] leftBoundary = new int[heights.length];
+    int[] rightBoundary = new int[heights.length];
+    // compute left boundary
+    leftBoundary[0] = -1;
+    for (int i = 1; i < heights.length; i++) {
+      int left = i - 1;
+      while (left >= 0 && heights[left] >= heights[i]) {
+        left = leftBoundary[left];
+      }
+      leftBoundary[i] = left;
+    }
+    // compute right boundary
+    rightBoundary[heights.length - 1] = heights.length;
+    for (int i = heights.length - 2; i >= 0; i--) {
+      int right = i + 1;
+      while (right < heights.length && heights[right] >= heights[i]) {
+        right = rightBoundary[right];
+      }
+      rightBoundary[i] = right;
+    }
+    int max = 0;
+    for (int i = 0; i < heights.length; i++) {
+      max = Math.max(max, heights[i] * (rightBoundary[i] - leftBoundary[i] - 1));
+    }
+    return max;
+  }
+
+  public static int divideConquerArea(int... heights) {
+    return process(0, heights.length - 1, heights);
+  }
+
+  /**
+   * Divide Conquer solution:
+   * idea is to to split the array into two parts, divide by min height
+   */
+  private static int process(int from, int to, int... heights) {
+    if (from > to) {
+      return 0;
+    } else if (from == to) {
+      return heights[from];
+    }
+    int minIndex = from;
+    for (int i = from + 1; i <= to; i++) {
+      if (heights[i] < heights[minIndex]) {
+        minIndex = i;
+      }
+    }
+    int max = (to + 1 - from) * heights[minIndex];
+    int leftMax = process(from, minIndex - 1, heights);
+    int rightMax = process(minIndex + 1, to, heights);
+    return Math.max(max, Math.max(leftMax, rightMax));
+  }
+
   public static int area(int... heights) {
     // currently the merged rectangle width
     int[] rectangleWidth = new int[heights.length];
--- LeetCode/src/test/java/com/loic/leetcode/hard/LargestRectangleInHistogramTest.java
+++ LeetCode/src/test/java/com/loic/leetcode/hard/LargestRectangleInHistogramTest.java
@@ -11,4 +11,18 @@ class LargestRectangleInHistogramTest {
     Assertions.assertEquals(9, LargestRectangleInHistogram.area(9));
     Assertions.assertEquals(10, LargestRectangleInHistogram.area(2, 1, 5, 6, 2, 3));
   }
+
+  @Test
+  void divideConquerArea() {
+    Assertions.assertEquals(0, LargestRectangleInHistogram.divideConquerArea());
+    Assertions.assertEquals(9, LargestRectangleInHistogram.divideConquerArea(9));
+    Assertions.assertEquals(10, LargestRectangleInHistogram.divideConquerArea(2, 1, 5, 6, 2, 3));
+  }
+
+  @Test
+  void boundaryArea() {
+    Assertions.assertEquals(0, LargestRectangleInHistogram.boundaryArea());
+    Assertions.assertEquals(9, LargestRectangleInHistogram.boundaryArea(9));
+    Assertions.assertEquals(10, LargestRectangleInHistogram.boundaryArea(2, 1, 5, 6, 2, 3));
+  }
 }
