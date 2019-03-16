168061a58a01d5cfbdfaf54bb8536ca53bd8fbdb
xiandong <xdzhou.loic@gmail.com>
2019-03-16 08:13:19 +0100

    create tree node from level order

--- LeetCode/src/main/java/com/loic/leetcode/helper/TreeNode.java
+++ LeetCode/src/main/java/com/loic/leetcode/helper/TreeNode.java
@@ -1,11 +1,70 @@
 package com.loic.leetcode.helper;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class TreeNode {
-  public final int val;
+  public int val;
   public TreeNode left;
   public TreeNode right;
 
   public TreeNode(int x) {
     val = x;
+  }
+
+  /**
+   * create a Binary tree from level order
+   */
+  public static TreeNode createFromLevelOrder(List<Integer> list) {
+    if (list.isEmpty()) {
+      return null;
+    }
+    TreeNode root = new TreeNode(list.get(0));
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    int index = 1;
+    while (!queue.isEmpty()) {
+      int nodeSize = queue.size();
+      for (int i = 0; i < nodeSize; i++) {
+        TreeNode node = queue.poll();
+        TreeNode left = generate(list, index++);
+        TreeNode right = generate(list, index++);
+        node.left = left;
+        node.right = right;
+        if (left != null) {
+          queue.add(left);
+        }
+        if (right != null) {
+          queue.add(right);
+        }
+      }
+    }
+    return root;
+  }
+
+  private static TreeNode generate(List<Integer> list, int index) {
+    return (index >= list.size() || list.get(index) == null) ?
+      null : new TreeNode(list.get(index));
+  }
+
+  @Override
+  public String toString() {
+    List<Integer> levelOrder = new ArrayList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(this);
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+      levelOrder.add(node == null ? null : node.val);
+      if (node != null) {
+        queue.add(node.left);
+        queue.add(node.right);
+      }
+    }
+    while (levelOrder.size() > 0 && levelOrder.get(levelOrder.size() - 1) == null) {
+      levelOrder.remove(levelOrder.size() - 1);
+    }
+    return levelOrder.toString();
   }
 }
---
+++ LeetCode/src/test/java/com/loic/leetcode/helper/TreeNodeTest.java
@@ -0,0 +1,23 @@
+package com.loic.leetcode.helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TreeNodeTest {
+
+  @Test
+  void createFromLevelOrder() {
+    Assertions.assertNull(TreeNode.createFromLevelOrder(Arrays.asList()));
+
+    List<Integer> list = Arrays.asList(1, 2, 2, 3, 4, 4, 3);
+
+    Assertions.assertEquals(list.toString(), TreeNode.createFromLevelOrder(list).toString());
+
+    list = Arrays.asList(1, null, 2, null, 3);
+    Assertions.assertEquals(list.toString(), TreeNode.createFromLevelOrder(list).toString());
+  }
+
+}
